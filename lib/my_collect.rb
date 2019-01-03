e5ef91bb685545cb4d83200af3210eefc0471f82
Jordan Ginor <jordan.ginor@gmail.com>
2019-01-03 08:05:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,8 @@
 def my_collect(collection)
   i = 0
   while i < collection.length
-    yield(collection[i])
+    yield(collection[i].upcase)
     i += 1
   end
-  collection.upcase
+  collection
 end 
